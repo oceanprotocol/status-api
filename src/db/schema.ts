3307aab97a0004c48fe01ab24d6cff43213974b7
Jamie Hewitt <jamie.hewitt15@gmail.com>
2022-10-12 13:16:06 +0300

    Updating schema types

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { Schema } from 'mongoose'
 import { Status, State } from '../@types'
-import { BigNumber } from 'ethers'
+import BigNumberSchema from 'mongoose-bignumber'
 
 export const statusSchema = new Schema<Status>({
   network: { type: String, required: true },
@@ -8,15 +8,25 @@ export const statusSchema = new Schema<Status>({
   market: { type: String, enum: State, required: true, default: State.Warning },
   port: { type: String, enum: State, required: true, default: State.Warning },
   faucet: {
-    status: { type: String, enum: State, required: true },
+    status: {
+      type: String,
+      enum: State,
+      required: true,
+      default: State.Warning
+    },
     response: Number,
-    // ethBalance: BigNumber,
+    ethBalance: BigNumberSchema,
     ethBalanceSufficient: Boolean,
-    // oceanBalance: BigNumber,
+    oceanBalance: BigNumberSchema,
     oceanBalanceSufficient: Boolean
   },
   aquarius: {
-    status: { type: String, enum: State, required: true },
+    status: {
+      type: String,
+      enum: State,
+      required: true,
+      default: State.Warning
+    },
     response: Number,
     validChainList: Boolean,
     version: String,
@@ -26,27 +36,52 @@ export const statusSchema = new Schema<Status>({
     validQuery: Boolean
   },
   provider: {
-    status: { type: String, enum: State, required: true },
+    status: {
+      type: String,
+      enum: State,
+      required: true,
+      default: State.Warning
+    },
     response: Number,
     version: String,
     latestRelease: String
   },
   subgraph: {
-    status: { type: String, enum: State, required: true },
+    status: {
+      type: String,
+      enum: State,
+      required: true,
+      default: State.Warning
+    },
     response: Number,
     version: String,
     latestRelease: String,
     block: Number
   },
   operator: {
-    status: { type: String, enum: State, required: true },
+    status: {
+      type: String,
+      enum: State,
+      required: true,
+      default: State.Warning
+    },
     response: Number,
     version: String,
     latestRelease: String,
     environments: Number,
     limitReached: Boolean
   },
-  dataFarming: { type: String, enum: State, required: true },
-  daoGrants: { type: String, enum: State, required: true },
+  dataFarming: {
+    type: String,
+    enum: State,
+    required: true,
+    default: State.Warning
+  },
+  daoGrants: {
+    type: String,
+    enum: State,
+    required: true,
+    default: State.Warning
+  },
   lastUpdatedOn: { type: Number, required: true, default: Date.now }
 })
